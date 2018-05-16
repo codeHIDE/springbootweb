0c71821a388a0223cb8da1ca3b75ad03f207051c
codeHIDE <248780081@qq.com>
2018-05-16 16:06:21 +0800

    websocket

--- src/main/java/com/jyc/config/WebConfig.java
+++ src/main/java/com/jyc/config/WebConfig.java
@@ -3,7 +3,6 @@ package com.jyc.config;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
@@ -11,22 +10,19 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.jyc.filter.TimeFilter;
-import com.jyc.interceptor.TimeInterceptor;
 import com.jyc.listener.ListenerTest;
 import com.jyc.servlet.ServletTest;
 
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig {
 
-	@Autowired
-    private TimeInterceptor timeInterceptor;
+//	@Autowired
+//    private TimeInterceptor timeInterceptor;
 	
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
@@ -67,9 +63,9 @@ public class WebConfig extends WebMvcConfigurerAdapter{
         return new ServletListenerRegistrationBean<ListenerTest>(new ListenerTest());
     }
     
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(timeInterceptor);
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(timeInterceptor);
+//    }
     
 }
