08898b4fc0823852b278267ea86a17cca5f90e73
codeHIDE <248780081@qq.com>
2018-05-16 11:42:50 +0800

    过滤器 拦截器

--- src/main/java/com/jyc/config/WebConfig.java
+++ src/main/java/com/jyc/config/WebConfig.java
@@ -1,19 +1,33 @@
 package com.jyc.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.jyc.filter.TimeFilter;
+import com.jyc.interceptor.TimeInterceptor;
+import com.jyc.listener.ListenerTest;
 import com.jyc.servlet.ServletTest;
 
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter{
 
+	@Autowired
+    private TimeInterceptor timeInterceptor;
+	
     @Bean
     public HttpMessageConverters fastJsonHttpMessageConverters() {
         FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
@@ -33,4 +47,29 @@ public class WebConfig {
     public ServletRegistrationBean servletRegistrationBean() {
         return new ServletRegistrationBean(new ServletTest(),"/servletTest");
     }
+    
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+        
+        TimeFilter timeFilter = new TimeFilter();
+        registrationBean.setFilter(timeFilter);
+        
+        List<String> urls = new ArrayList<>();
+        urls.add("/*");
+        registrationBean.setUrlPatterns(urls);
+        
+        return registrationBean;
+    }
+    
+    @Bean
+    public ServletListenerRegistrationBean<ListenerTest> servletListenerRegistrationBean() {
+        return new ServletListenerRegistrationBean<ListenerTest>(new ListenerTest());
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
+    
 }
