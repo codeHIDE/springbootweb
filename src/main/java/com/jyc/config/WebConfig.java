8ee6a73fb100e00b4f656b857fc9ca5e3eb97a10
codeHIDE <248780081@qq.com>
2018-05-16 11:06:54 +0800

    spring boot web

---
+++ src/main/java/com/jyc/config/WebConfig.java
@@ -0,0 +1,36 @@
+package com.jyc.config;
+
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.jyc.servlet.ServletTest;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastJsonHttpMessageConverter = new FastJsonHttpMessageConverter();
+
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        
+        fastJsonHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
+        
+        HttpMessageConverter<?> converter = fastJsonHttpMessageConverter;
+        
+        return new HttpMessageConverters(converter);
+
+    }
+    
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new ServletTest(),"/servletTest");
+    }
+}
