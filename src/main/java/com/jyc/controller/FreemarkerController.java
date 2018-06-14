0971d15fa08c5ce751a738f7a68444ccacadb419
codeHIDE <248780081@qq.com>
2018-06-14 14:19:19 +0800

    update

--- src/main/java/com/jyc/controller/FreemarkerController.java
+++ src/main/java/com/jyc/controller/FreemarkerController.java
@@ -4,12 +4,13 @@ import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("freemarker")
 public class FreemarkerController {
 
-    @RequestMapping("hello")
+    @RequestMapping(value = "hello", method = RequestMethod.GET)  
     public String hello(Map<String,Object> map) {
         
         map.put("msg", "Hello Freemarker");
